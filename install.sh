bb2842925a1cdf2957b7c4bbf6033409da30e7ac
Daniel Platz <daniel.platz@me.com>
2018-05-20 10:25:34 +0200

    typo

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/tmux-conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vimrc.bundles ~/.vimrc.bundles
 ln -s ~/dotfiles/zshrc ~/.zshrc
