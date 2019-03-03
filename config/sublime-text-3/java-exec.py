264376e114a18ffd827a134ff46995270f8310a8
Daniel Platz <daniel.platz@me.com>
2019-03-03 19:35:06 +0100

    tabs -> whitespaces

--- config/sublime-text-3/java-exec.py
+++ config/sublime-text-3/java-exec.py
@@ -1,16 +1,16 @@
 import sublime
 import sublime_plugin
 import subprocess
-import os
+//import os
 
 class JavaExecCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		view = self.view
-		file = view.window().active_view().file_name()
-		filename = os.path.basename(file)
-		classname = os.path.splitext(filename)[0]
-		dirname = os.path.dirname(file)
-		view.run_command('save')
-		result = subprocess.check_output('cd %s && javac %s && java %s' % (dirname, filename, classname), stderr=subprocess.STDOUT, shell=True)
-		html_result = "<br />".join(result.decode("utf-8").split("\n"))
-		view.show_popup(html_result, location=-1, max_width=600)
+    def run(self, edit):
+        view = self.view
+        file = view.window().active_view().file_name()
+        filename = os.path.basename(file)
+        classname = os.path.splitext(filename)[0]
+        dirname = os.path.dirname(file)
+        view.run_command('save')
+        result = subprocess.check_output('cd %s && javac %s && java %s' % (dirname, filename, classname), stderr=subprocess.STDOUT, shell=True)
+        html_result = "<br />".join(result.decode("utf-8").split("\n"))
+        view.show_popup(html_result, location=-1, max_width=600)
