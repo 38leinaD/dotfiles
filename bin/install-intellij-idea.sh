703a71221813c4888546d8c333dfc615f1b93c6e
Daniel Platz <daniel.platz@dieboldnixdorf.com>
2023-12-21 13:15:34 +0100

    idea download improved

--- bin/install-intellij-idea.sh
+++ bin/install-intellij-idea.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -e
-pushd ~/Downloads
-curl -LO https://download.jetbrains.com/idea/ideaIU-2023.3.1.tar.gz
-sudo tar -xzf ideaIU-*.tar.gz -C /opt
-sudo ln -s /opt/idea-IU-233.11799.300/bin/idea.sh /usr/local/bin/idea
 
+sudo echo
 
+IDEA_DOWNLOAD_LINK=$(curl https://data.services.jetbrains.com/products\?code\=IIU\&release.type\=eap%2Crc%2Crelease\&fields\=distributions%2Clink%2Cname%2Creleases | jq -r '.[0].releases[0].downloads.linux.link')
 
-popd
+IDEA_TAR=$(echo $IDEA_DOWNLOAD_LINK | rev | cut -d / -f1 | rev)
+
+if [ ! -e ~/Downloads/$IDEA_TAR ]; then
+	pushd ~/Downloads
+	curl -LO $IDEA_DOWNLOAD_LINK
+	popd
+fi
+
+TMP=$(mktemp -d)
+echo "Using temporary directory $TMP"
+
+tar -xzf ~/Downloads/$IDEA_TAR -C $TMP
+IDEA_DIR=$(ls $TMP)
+sudo mv $TMP/$IDEA_DIR /opt/
+sudo rm -f /usr/local/bin/idea
+sudo ln -s /opt/$IDEA_DIR/bin/idea.sh /usr/local/bin/idea
