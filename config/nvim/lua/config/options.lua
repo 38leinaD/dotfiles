872753285bfe9e7e8d735f4bd3bed299a24961f5
Daniel Platz <daniel.platz@dieboldnixdorf.com>
2025-11-24 14:54:18 +0100

    updates

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -9,3 +9,5 @@ vim.opt.expandtab = true     -- Use spaces instead of tabs
 vim.opt.shiftwidth = 4       -- Indent by 2 spaces
 vim.opt.tabstop = 4          -- A tab counts as 2 spaces
 vim.opt.softtabstop = 4      -- Pressing <Tab> inserts 2 spaces
+
+vim.g.lazyvim_picker = "telescope"
